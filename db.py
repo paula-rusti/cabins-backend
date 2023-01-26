fc0497f069f3eb2292be151e7621e2a0c58acacf
Paula Rusti <rusti.paula78@gmail.com>
2023-01-26 21:34:37 +0200

    added user_model and function to get the cached session for writing users to the users table

---
+++ db.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+
+from sqlmodel import create_engine, SQLModel
+from sqlmodel import Session
+
+user = 'app'
+password = 'app'
+host = 'localhost'
+port = 5432
+database = 'mydbname'
+
+url = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
+    user, password, host, port, database
+)
+
+
+@lru_cache()
+def get_db_session():
+    engine = create_engine(url, echo=True)
+    SQLModel.metadata.create_all(engine)
+    session = Session(bind=engine)
+    return session
