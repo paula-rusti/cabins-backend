03667b6b1ac9d9842392c3fe7d4e2249ab7b63f3
Paula Rusti <rusti.paula78@gmail.com>
2023-03-18 13:48:42 +0200

    added booking router

---
+++ routers/booking_router.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+import models.orm_models
+from db import get_db
+import models.dto_models, models.orm_models
+
+router = APIRouter(prefix="/bookings", tags=["bookings"])
+# route order matters, wildcards should be placed last
+
+
+@router.post("/add")
+def add_booking(
+    booking: models.dto_models.BookingCreate, db: Session = Depends(get_db)
+):
+    to_add = models.orm_models.Booking(**booking.dict())
+    db.add(to_add)
+    db.commit()
